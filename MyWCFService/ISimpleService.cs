ac5e62b4f2c1256d0d0d7db5829ec3a727a01b1f
RandyCheng <linusche@gmail.com>
2018-02-27 15:54:15 +0800

    新增wcf服務

---
+++ MyWCFService/ISimpleService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWCFService
+{
+    //這個cs檔案是wcf的介面
+    //請在svc.cs執行測試
+
+
+    // 注意: 您可以使用 [重構] 功能表上的 [重新命名] 命令同時變更程式碼和組態檔中的介面名稱 "ISimpleService"。
+    [ServiceContract]
+    public interface ISimpleService
+    {
+        [OperationContract]//公開在網路上
+        double GetStockPrice(int CompanyID);
+    }
+}
---
+++ MyWCFService/SimpleService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWCFService
+{
+    //這邊是實作ISimpleService的類別
+    //要在這邊執行 叫用出WCF測試用戶端
+
+    // 注意: 您可以使用 [重構] 功能表上的 [重新命名] 命令同時變更程式碼、svc 和組態檔中的類別名稱 "SimpleService"。
+    // 注意: 若要啟動 WCF 測試用戶端以便測試此服務，請在 [方案總管] 中選取 SimpleService.svc 或 SimpleService.svc.cs，然後開始偵錯。
+    public class SimpleService : ISimpleService
+    {
+
+
+        public double GetStockPrice(int CompanyID)
+        {
+            Random r = new Random();
+            return r.NextDouble() * r.Next(1, 101);
+        }
+    }
+}
